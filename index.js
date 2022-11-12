63143df56e72089684c90acf11266f17cf5c1456
Hamees Sayed <hameessayed71@gmail.com>
2022-11-12 20:06:04 +0530

    added frontend

--- index.js
+++ index.js
@@ -1,23 +1,56 @@
 import { menuArray } from './data.js'
 
-const menu = document.getElementsByClassName('menu')
+document.addEventListener('click', (e) => {
+    if (e.target.id) {
+        handleAddToCart(e.target.id)
+    }
+})
 
-function showMenu(menuArray) {
+function handleAddToCart(addId) {
+    console.log(addId)
 
-    
+    const item = menuArray.filter((foodItem) => {
+        return foodItem.id == addId 
+    })[0]
 
-    menu.innerHTML += `
-    <div class="item">
-    <div class="txt">
-       <p class="img">🍕</p>
-       <div>
-          <p class="food">Pizza</p>
-          <p class="ingredient">pepperoni,mushroom,pizza</p>
-          <p class="cost">$14</p>
-       </div>
+    console.log(item)
+
+    let orderCart = ``
+
+    orderCart = `
+    <div class="content">
+        <p class="cart">${item.name}</p>
+        <p class="remove">remove</p>
     </div>
-    <div class="btn">+</div>
-    <div class="line"></div>
- </div>
-    `
+    <p>${item.price}</p>`
+
+    document.querySelector('.order-item').innerHTML += orderCart
 }
+
+function showMenu() {
+
+    let menuHTML = ``
+
+    menuArray.forEach(function (item) {
+        menuHTML += `
+        <div class="item">
+            <div class="txt">
+               <p class="img">${item.emoji}</p>
+               <div>
+                  <p class="food">${item.name}</p>
+                  <p class="ingredient">${item.ingredients}</p>
+                  <p class="cost">$${item.price}</p>
+               </div>
+            </div>
+            <div class="btn" id=${item.id}>+</div>
+            <div class="line"></div>
+        </div>`
+    })
+    return menuHTML
+}
+
+function render() {
+    document.querySelector('.menu').innerHTML = showMenu()
+}
+
+render()
